0b9e3c99bd3e69f6b3ffb3be1f8d4b66ff6b4ea5
Proselin94 <quochung9401@gmail.com>
2022-05-14 01:05:45 +0700

    create

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config({path: "./.env"})
 
-app.listen(port);
+//swagger ui        
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-console.log('RESTful API server started on: ' + port);
+//port
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+let routes = require('./api/routes') //importing route
+routes(app)
+
+// app use
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options))
+
+app.listen(port)
+
+var options = {
+    swaggerOptions: {
+            url: 'http://localhost:3000/v2/swagger.json'
+    }
+}
+
+console.log('RESTful API server started on: ' + port)
